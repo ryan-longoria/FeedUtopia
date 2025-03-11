576dc215c744e2ef7f36ed9aa65e1b396f780883
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-11 14:00:50 -0500

    Correct lambda handler name

--- accounts/animeutopia-prod/lambda.tf
+++ accounts/animeutopia-prod/lambda.tf
@@ -10,7 +10,7 @@ resource "aws_lambda_function" "fetch_rss" {
   function_name      = "fetch_rss"
   filename           = "${path.module}/artifacts/scripts/fetch_rss/fetch_rss.zip"
   source_code_hash   = filebase64sha256("${path.module}/artifacts/scripts/fetch_rss/fetch_rss.zip")
-  handler            = "fetch_rss.lambda_handler"
+  handler            = "lambda_function.lambda_handler"
   runtime            = "python3.9"
   role               = aws_iam_role.lambda_role.arn
   timeout            = 10
@@ -24,7 +24,7 @@ resource "aws_lambda_function" "process_content" {
   function_name      = "process_content"
   filename           = "${path.module}/artifacts/scripts/process_content/process_content.zip"
   source_code_hash   = filebase64sha256("${path.module}/artifacts/scripts/process_content/process_content.zip")
-  handler            = "process_content.lambda_handler"
+  handler            = "lambda_function.lambda_handler"
   runtime            = "python3.9"
   role               = aws_iam_role.lambda_role.arn
   timeout            = 10
@@ -46,7 +46,7 @@ resource "aws_lambda_function" "store_data" {
   function_name      = "store_data"
   filename           = "${path.module}/artifacts/scripts/store_data/store_data.zip"
   source_code_hash   = filebase64sha256("${path.module}/artifacts/scripts/store_data/store_data.zip")
-  handler            = "store_data.lambda_handler"
+  handler            = "lambda_function.lambda_handler"
   runtime            = "python3.9"
   role               = aws_iam_role.lambda_role.arn
   timeout            = 10
@@ -65,7 +65,7 @@ resource "aws_lambda_function" "render_video" {
   function_name      = "render_video"
   filename           = "${path.module}/artifacts/scripts/render_video/render_video.zip"
   source_code_hash   = filebase64sha256("${path.module}/artifacts/scripts/render_video/render_video.zip")
-  handler            = "render_video.lambda_handler"
+  handler            = "lambda_function.lambda_handler"
   runtime            = "python3.9"
   role               = aws_iam_role.lambda_role.arn
   timeout            = 180
@@ -89,7 +89,7 @@ resource "aws_lambda_function" "notify_post" {
   function_name      = "notify_post"
   filename           = "${path.module}/artifacts/scripts/notify_post/notify_post.zip"
   source_code_hash   = filebase64sha256("${path.module}/artifacts/scripts/notify_post/notify_post.zip")
-  handler            = "notify_post.lambda_handler"
+  handler            = "lambda_function.lambda_handler"
   runtime            = "python3.9"
   role               = aws_iam_role.lambda_role.arn
   timeout            = 10
