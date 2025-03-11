556513b6bf4b2f142d0d052598dfc50534a31b78
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-11 08:25:43 -0500

    Add moviepy_layer lambda layer resource

---
+++ accounts/animeutopia-prod/lambda_layers.tf
@@ -0,0 +1,6 @@
+resource "aws_lambda_layer_version" "moviepy_layer" {
+  layer_name          = "moviepy-layer"
+  filename            = "${path.module}/moviepy_layer.zip"
+  compatible_runtimes = ["python3.9"]
+  description         = "MoviePy and FFmpeg dependencies"
+}
