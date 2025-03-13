1f769d1504037e5c608eea51012a772a03aa50e4
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-13 11:20:37 -0500

    Move NEWS clip to (0,0)

--- accounts/animeutopia-prod/artifacts/scripts/render_video/lambda_function.py
+++ accounts/animeutopia-prod/artifacts/scripts/render_video/lambda_function.py
@@ -142,7 +142,7 @@ def lambda_handler(event, context):
         raw_news = VideoFileClip(news_local_path).with_duration(duration_sec)
         scale_news = 500 / raw_news.w
         news_clip = raw_news.with_effects([vfx.Resize(scale_news)])
-        news_clip = news_clip.transform(lambda gf, t: remove_white(gf(t))).with_position((10, 10))
+        news_clip = news_clip.transform(lambda gf, t: remove_white(gf(t))).with_position((0, 0))
     else:
         news_clip = None
 
