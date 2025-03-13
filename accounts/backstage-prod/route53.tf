2987b9610b46edd95d65a5c641810ec99f72c0f8
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-13 18:06:07 -0500

    Add tf for route53

---
+++ accounts/backstage-prod/route53.tf
@@ -0,0 +1,33 @@
+resource "aws_route53_zone" "feedutopia" {
+  name = "feedutopia.com"
+}
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "backstage.feedutopia.com"
+  validation_method = "DNS"
+}
+
+resource "aws_route53_record" "cert_validation" {
+  name    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_name
+  type    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_type
+  zone_id = aws_route53_zone.feedutopia.zone_id
+  records = [aws_acm_certificate.cert.domain_validation_options[0].resource_record_value]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "cert_validation_complete" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+}
+
+resource "aws_route53_record" "backstage_domain" {
+  zone_id = aws_route53_zone.feedutopia.zone_id
+  name    = "backstage.feedutopia.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.frontend.dns_name
+    zone_id                = aws_lb.frontend.zone_id
+    evaluate_target_health = true
+  }
+}
