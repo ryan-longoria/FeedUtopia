2d28d046f5fa7fc428ac04b40845ce481da14b8e
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-13 17:58:54 -0500

    Define ingress and egress rules as nested blocks

--- accounts/backstage-prod/alb.tf
+++ accounts/backstage-prod/alb.tf
@@ -2,31 +2,45 @@ resource "aws_security_group" "alb" {
   name        = "backstage-alb-sg"
   description = "Allow inbound HTTP/HTTPS to ALB"
   vpc_id      = aws_vpc.main.id
-  ingress = [
-    {
-      description = "Allow HTTP"
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      description = "Allow HTTPS"
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
-  egress = [
-    {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
-  tags = { Name = "backstage-alb-sg" }
+
+  ingress {
+    description      = "Allow HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    security_groups  = []
+    self             = false
+  }
+
+  ingress {
+    description      = "Allow HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    security_groups  = []
+    self             = false
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    security_groups  = []
+    self             = false
+  }
+
+  tags = {
+    Name = "backstage-alb-sg"
+  }
 }
 
 resource "aws_security_group" "ecs" {
