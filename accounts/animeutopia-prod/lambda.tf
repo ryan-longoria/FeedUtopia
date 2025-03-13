e95a2c1a4af2e992ae178644fb40149b6925213c
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-12 20:00:20 -0500

    Modify image_uri

--- accounts/animeutopia-prod/lambda.tf
+++ accounts/animeutopia-prod/lambda.tf
@@ -64,7 +64,7 @@ resource "aws_lambda_function" "store_data" {
 resource "aws_lambda_function" "render_video" {
   function_name = "render_video"
   package_type  = "Image"
-  image_uri     = "481665084477.dkr.ecr.us-east-2.amazonaws.com/render_video_repository@sha256:44dbf43e2e552af107befd0ab9ba69ba5ce60824a9b3ef86acb19107460a42b2"
+  image_uri     = "481665084477.dkr.ecr.us-east-2.amazonaws.com/render_video_repository@sha256:c0c9ac100ea3d6b5b8bd646b817ed454d7b6426ee1147c881b9f0eeb5284b589"
   role          = aws_iam_role.lambda_role.arn
   timeout       = 300
   memory_size   = 1024
