30715998ef0f5847ab85f3dc2de42971486e13a9
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-14 09:19:45 -0500

    Define ingress and egress rules as nested blocks

--- accounts/backstage-prod/rds.tf
+++ accounts/backstage-prod/rds.tf
@@ -2,25 +2,36 @@ resource "aws_security_group" "rds" {
   name        = "backstage-rds-sg"
   description = "Allow ECS tasks to access RDS PostgreSQL"
   vpc_id      = aws_vpc.main.id
-  ingress = [
-    {
-      description     = "Postgres from ECS tasks"
-      from_port       = 5432
-      to_port         = 5432
-      protocol        = "tcp"
-      security_groups = [aws_security_group.ecs.id]
-    }
-  ]
-  egress = [
-    {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
-  tags = { Name = "backstage-rds-sg" }
+
+  ingress {
+    description      = "Postgres from ECS tasks"
+    from_port        = 5432
+    to_port          = 5432
+    protocol         = "tcp"
+    security_groups  = [aws_security_group.ecs.id]
+    cidr_blocks      = []             # Explicitly provide an empty list
+    ipv6_cidr_blocks = []             # if not used
+    prefix_list_ids  = []             # if not used
+    self             = false
+  }
+
+  egress {
+    description      = "Allow all outbound traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []             # Explicitly provide an empty list
+    prefix_list_ids  = []             # if not used
+    security_groups  = []             # if not used
+    self             = false
+  }
+
+  tags = {
+    Name = "backstage-rds-sg"
+  }
 }
+
 
 resource "aws_db_subnet_group" "backstage" {
   name       = "backstage-db-subnet-group"
