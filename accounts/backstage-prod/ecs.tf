8157c7608e2acc607393f259be7d870faf841966
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-14 17:53:18 -0500

    Revert tocommit d6de89a7d44a8758670e084a5ddacc2154d5b8ba and modify ports and image uri

--- accounts/backstage-prod/ecs.tf
+++ accounts/backstage-prod/ecs.tf
@@ -8,24 +8,31 @@ resource "aws_cloudwatch_log_group" "backstage" {
 }
 
 resource "aws_ecs_task_definition" "backstage" {
-  family                   = "${var.app_name}-task"
+  family                   = "backstage-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "512"
   memory                   = "1024"
-  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
-  task_role_arn            = aws_iam_role.backstage_task.arn
+  execution_role_arn       = aws_iam_role.ecs_task_exec.arn
+  task_role_arn            = aws_iam_role.ecs_task.arn
+
   container_definitions = jsonencode([
     {
-      name         = "backstage"
-      image        = "825765422855.dkr.ecr.us-east-2.amazonaws.com/backstage-repo:latest"
-      portMappings = [{ containerPort = 7000, hostPort = 7000 }]
+      name  = "backstage"
+      image = "825765422855.dkr.ecr.us-east-2.amazonaws.com/backstage-repo:latest"
+      portMappings = [
+        {
+          containerPort = 700
+          hostPort      = 700
+        }
+      ]
       environment = [
         { name : "NODE_ENV", value : "production" },
-        { name : "PGHOST", value : aws_db_instance.backstage.address },
-        { name : "PGUSER", value : var.db_username },
-        { name : "PGPASSWORD", value : var.db_password },
-        { name : "PGDATABASE", value : var.db_name },
+        { name = "POSTGRES_HOST", value = aws_db_instance.backstage.address },
+        { name = "POSTGRES_USER", value = aws_db_instance.backstage.username },
+        { name = "POSTGRES_PASSWORD", value = var.db_password },
+        { name = "POSTGRES_DATABASE", value = aws_db_instance.backstage.db_name },
+        { name = "TECHDOCS_S3_BUCKET", value = aws_s3_bucket.techdocs.id },
         { name : "APP_CONFIG_app_baseUrl", value : "https://${var.backstage_domain}" },
         { name : "APP_CONFIG_backend_baseUrl", value : "https://${var.backstage_domain}" },
         { name : "AWS_REGION", value : var.aws_region }
@@ -33,9 +40,9 @@ resource "aws_ecs_task_definition" "backstage" {
       logConfiguration = {
         logDriver = "awslogs"
         options = {
-          awslogs-group         = "/ecs/${var.app_name}"
+          awslogs-group         = aws_cloudwatch_log_group.backstage.name
           awslogs-region        = var.aws_region
-          awslogs-stream-prefix = "ecs"
+          awslogs-stream-prefix = "backstage"
         }
       }
     }
@@ -43,21 +50,23 @@ resource "aws_ecs_task_definition" "backstage" {
 }
 
 resource "aws_ecs_service" "backstage" {
-  name             = "${var.app_name}-service"
-  cluster          = aws_ecs_cluster.backstage.id
-  task_definition  = aws_ecs_task_definition.backstage.arn
-  launch_type      = "FARGATE"
-  desired_count    = 1
-  platform_version = "LATEST"
+  name            = "backstage-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.backstage.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
   network_configuration {
-    subnets          = [for subnet in aws_subnet.private : subnet.id]
-    security_groups  = [aws_security_group.ecs_sg.id]
+    subnets          = [aws_subnet.private1.id, aws_subnet.private2.id]
+    security_groups  = [aws_security_group.ecs.id]
     assign_public_ip = false
   }
+
   load_balancer {
     target_group_arn = aws_lb_target_group.backstage.arn
     container_name   = "backstage"
     container_port   = 7000
   }
-  depends_on = [aws_lb_listener_rule.cognito_auth]
+
+  depends_on = [aws_lb_listener.frontend_https]
 }
