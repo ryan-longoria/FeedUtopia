7511ed49b8a6f81597e6d2957fca1019ec2815bb
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-14 17:32:28 -0500

    Configure techdocs (mkdocs with s3 backend)

--- accounts/backstage-prod/s3.tf
+++ accounts/backstage-prod/s3.tf
@@ -26,3 +26,21 @@ resource "aws_s3_bucket_public_access_block" "techdocs_block" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket_policy" "techdocs" {
+  bucket = aws_s3_bucket.techdocs.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement: [
+      {
+        Effect: "Allow",
+        Principal: { AWS: aws_iam_role.backstage_task.arn },
+        Action: ["s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:ListBucket"],
+        Resource: [
+          "${aws_s3_bucket.techdocs.arn}",
+          "${aws_s3_bucket.techdocs.arn}/*"
+        ]
+      }
+    ]
+  })
+}
