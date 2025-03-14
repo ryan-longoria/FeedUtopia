29150d4f645de2948b95882b28d21e405ebf99b6
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-14 11:39:44 -0500

    Add certificate validation block for the alb

--- accounts/backstage-prod/route53.tf
+++ accounts/backstage-prod/route53.tf
@@ -7,6 +7,14 @@ resource "aws_acm_certificate" "cert" {
   validation_method = "DNS"
 }
 
+resource "aws_acm_certificate_validation" "cert_validation_complete" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+
+  validation_record_fqdns = [
+    aws_route53_record.cert_validation.fqdn
+  ]
+}
+
 resource "aws_route53_record" "cert_validation" {
   name    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_name
   type    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_type
