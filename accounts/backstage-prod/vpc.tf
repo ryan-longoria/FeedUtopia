c8f5dda4d8fea170d191400f26ac53878538ec3e
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-14 18:21:43 -0500

    modify subnet tags

--- accounts/backstage-prod/vpc.tf
+++ accounts/backstage-prod/vpc.tf
@@ -15,14 +15,14 @@ resource "aws_subnet" "public1" {
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-2a"
   map_public_ip_on_launch = true
-  tags                    = { Name = "${var.app_name}-public-1" }
+  tags                    = { Name = "backstage-public-1" }
 }
 resource "aws_subnet" "public2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-2b"
   map_public_ip_on_launch = true
-  tags                    = { Name = "${var.app_name}-public-2" }
+  tags                    = { Name = "backstage-public-2" }
 }
 
 resource "aws_subnet" "private1" {
@@ -30,14 +30,14 @@ resource "aws_subnet" "private1" {
   cidr_block              = "10.0.11.0/24"
   availability_zone       = "us-east-2a"
   map_public_ip_on_launch = false
-  tags                    = { Name = "${var.app_name}-private-1" }
+  tags                    = { Name = "backstage-private-1" }
 }
 resource "aws_subnet" "private2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.12.0/24"
   availability_zone       = "us-east-2b"
   map_public_ip_on_launch = false
-  tags                    = { Name = "${var.app_name}-private-2" }
+  tags                    = { Name = "backstage-private-2" }
 }
 
 resource "aws_eip" "nat_eip" {
