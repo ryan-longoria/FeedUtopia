4367a879bbb069a39d7af37d634b24499becb5d6
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-15 17:48:39 -0500

    Update availability zone to us-east-2

--- accounts/animeutopia-prod/network.tf
+++ accounts/animeutopia-prod/network.tf
@@ -17,7 +17,7 @@ resource "aws_internet_gateway" "example_igw" {
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.example_vpc.id
   cidr_block             = "10.0.0.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "us-east-2"
   map_public_ip_on_launch = true
 
   tags = {
@@ -28,7 +28,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.example_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "us-east-2"
 
   tags = {
     Name = "private-subnet"
