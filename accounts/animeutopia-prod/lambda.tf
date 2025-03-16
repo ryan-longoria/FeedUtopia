a21a2dd31a075c52060c6700a8463b52c6ce1607
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-16 00:45:27 -0500

    Add EFS config for render_video usage

--- accounts/animeutopia-prod/lambda.tf
+++ accounts/animeutopia-prod/lambda.tf
@@ -68,7 +68,7 @@ resource "aws_lambda_function" "store_data" {
 resource "aws_lambda_function" "render_video" {
   function_name = "render_video"
   package_type  = "Image"
-  image_uri     = "481665084477.dkr.ecr.us-east-2.amazonaws.com/render_video_repository@sha256:ce61feed21ec0ffd79c801817f87271cddcdc1ac4c500b4ac53fe96d5c7f17a1"
+  image_uri     = "481665084477.dkr.ecr.us-east-2.amazonaws.com/render_video_repository@sha256:178d0a89b62e44354be3cd77fc527e3b1404d7eff4143138aaa05d78e01f6c78"
   role          = aws_iam_role.lambda_role.arn
   timeout       = 300
   memory_size   = 3008
