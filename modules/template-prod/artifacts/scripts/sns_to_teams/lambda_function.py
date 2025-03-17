e1a1eb772406cd42020cde70d6cffabbca7919ff
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-17 13:39:20 -0500

    Handle event for Teams not giving a 200 with DLQ

--- modules/template-prod/artifacts/scripts/sns_to_teams/lambda_function.py
+++ modules/template-prod/artifacts/scripts/sns_to_teams/lambda_function.py
@@ -1,20 +1,33 @@
 import os
 import json
 import urllib3
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+http = urllib3.PoolManager()
 
 def lambda_handler(event, context):
     teams_webhook_url = os.environ['TEAMS_WEBHOOK_URL']
-    http = urllib3.PoolManager()
-
+    
     for record in event['Records']:
         sns_message = record['Sns']['Message']
-        
+
         body = {
             "text": f"A CloudWatch Alarm has triggered:\n\n{sns_message}"
         }
-        
+
         encoded_body = json.dumps(body).encode('utf-8')
-        resp = http.request("POST", teams_webhook_url, 
-                            body=encoded_body,
-                            headers={'Content-Type': 'application/json'})
-        print(f"Teams responded with status {resp.status}")
+        resp = http.request(
+            "POST",
+            teams_webhook_url,
+            body=encoded_body,
+            headers={'Content-Type': 'application/json'}
+        )
+        
+        logger.info(f"Teams responded with status {resp.status}")
+        
+        if not 200 <= resp.status < 300:
+            logger.error(f"Error posting to Teams: {resp.status}, {resp.data}")
+            raise Exception(f"Failed to post message to Teams: {resp.status}")
