ace16b3c25bcda5d227ca9233f549ca9d26ba51f
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-18 16:47:10 -0500

    Add A record for api.feedutopia.com

--- accounts/sharedservices-prod/route53.tf
+++ accounts/sharedservices-prod/route53.tf
@@ -11,3 +11,15 @@ data "aws_route53_zone" "feedutopia_zone" {
   name         = "feedutopia.com."
   private_zone = false
 }
+
+resource "aws_route53_record" "api_feedutopia_alias" {
+  zone_id = data.aws_route53_zone.feedutopia_zone.zone_id
+  name    = "api.feedutopia.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_api_gateway_domain_name.api_feedutopia_domain.regional_domain_name
+    zone_id                = aws_api_gateway_domain_name.api_feedutopia_domain.regional_zone_id
+    evaluate_target_health = false
+  }
+}
