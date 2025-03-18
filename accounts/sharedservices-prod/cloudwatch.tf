a4439a9189233e47be9dfe79ea4ae2aef233d830
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-18 16:36:14 -0500

    ran terraform fmt

--- accounts/sharedservices-prod/cloudwatch.tf
+++ accounts/sharedservices-prod/cloudwatch.tf
@@ -2,7 +2,6 @@
 ## Cloudwatch
 ################################################################################
 
-
 resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
   name              = "/aws/vpc/flow_logs/${aws_vpc.API_vpc.id}"
   retention_in_days = 2
