cdbe4c8a353c247b092a2d948620f4169cb15b81
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-18 18:25:12 -0500

    Add depends_on for tf to wait until the integration in created.

--- accounts/sharedservices-prod/api.tf
+++ accounts/sharedservices-prod/api.tf
@@ -8,6 +8,10 @@ resource "aws_api_gateway_rest_api" "api" {
 
 resource "aws_api_gateway_deployment" "api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
+
+  depends_on = [
+    aws_api_gateway_integration.start_execution_integration
+  ]
 }
 
 resource "aws_api_gateway_stage" "api_stage" {
