7f2a6a9f403c67d7a017b425ce66851ae6408a9c
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-19 16:02:48 -0500

    Add depends_on to api deployment to ensure it goes after the method is created

--- accounts/sharedservices-prod/api.tf
+++ accounts/sharedservices-prod/api.tf
@@ -10,7 +10,8 @@ resource "aws_api_gateway_deployment" "api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
 
   depends_on = [
-    aws_api_gateway_integration.start_execution_integration
+    aws_api_gateway_integration.start_execution_integration,
+    aws_api_gateway_method.start_execution_post
   ]
 }
 
