9b18ccd53cd178887758f62fe5847d31a8c99b0f
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-18 21:24:36 -0500

    Fix typo syntax error

--- accounts/wrestleutopia-prod/sfn.tf
+++ accounts/wrestleutopia-prod/sfn.tf
@@ -8,7 +8,7 @@ resource "aws_sfn_state_machine" "automated_workflow" {
   type     = "EXPRESS"
 
   definition = templatefile("${path.module}/state_machine.json.tpl", {
-    fetch_rss_arn       = aws_lambda_function.fetch_data.arn,
+    fetch_data_arn       = aws_lambda_function.fetch_data.arn,
     process_content_arn = aws_lambda_function.process_content.arn,
     store_data_arn      = aws_lambda_function.store_data.arn,
     render_video_arn    = aws_lambda_function.render_video.arn,
