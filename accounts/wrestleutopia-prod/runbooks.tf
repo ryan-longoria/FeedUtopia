0bd6d5e8c1b46799d218b8c3024accae8d3b0591
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-20 07:48:01 -0500

    Delete runbook due to incompatibility with step functions put resource policy

--- accounts/wrestleutopia-prod/runbooks.tf
+++ accounts/wrestleutopia-prod/runbooks.tf
@@ -1,33 +1,3 @@
 ################################################################################
 ## Systems Manager (SSM) Automation Runbooks
 ################################################################################
-
-resource "aws_ssm_document" "attach_sfn_policy" {
-  name            = "AttachSFNResourcePolicy"
-  document_type   = "Automation"
-  document_format = "JSON"
-  depends_on      = [aws_iam_role.ssm_automation_role]
-
-  content = <<-DOC
-  {
-    "schemaVersion": "0.3",
-    "description": "Attach a resource-based policy to a Step Functions state machine (via Lambda)",
-    "assumeRole": "{{ AutomationAssumeRole }}",
-    "parameters": {
-      "ResourceArn":      { "type": "String" },
-      "PolicyB64":        { "type": "String" },
-      "AutomationAssumeRole": { "type": "String" }
-    },
-    "mainSteps": [
-      {
-        "action": "aws:invokeLambdaFunction",
-        "name": "put_sfn_policy",
-        "inputs": {
-          "FunctionName": "put_sfn_policy", 
-          "Payload": "{\"ResourceArn\": \"{{ ResourceArn }}\", \"PolicyB64\": \"{{ PolicyB64 }}\"}"
-        }
-      }
-    ]
-  }
-  DOC
-}
