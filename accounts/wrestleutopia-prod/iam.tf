3c86a10de7dc1181bcce5bf98ca31647860af6ec
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-19 20:25:01 -0500

    Create runbook for attaching a resource based policy to sfn every 12 hours

--- accounts/wrestleutopia-prod/iam.tf
+++ accounts/wrestleutopia-prod/iam.tf
@@ -350,16 +350,16 @@ EOF
 resource "aws_iam_role_policy" "ssm_automation_allow_putresourcepolicy" {
   name   = "SSMAutomationAllowPutResourcePolicy"
   role   = aws_iam_role.ssm_automation_role.id
+
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
         Effect   = "Allow",
-        Action   = [
-          "states:PutResourcePolicy"
-        ],
+        Action   = ["states:PutResourcePolicy"],
         Resource = "*"
       }
     ]
   })
 }
+
--- accounts/wrestleutopia-prod/runbooks.tf
+++ accounts/wrestleutopia-prod/runbooks.tf
@@ -12,14 +12,22 @@ resource "aws_ssm_document" "attach_sfn_policy" {
   "schemaVersion": "0.3",
   "description": "Attach Step Functions resource-based policy if missing",
   "parameters": {
-    "ResourceArn": { "type": "String" },
-    "PolicyJson":  { "type": "String" }
+    "ResourceArn": {
+      "type": "String"
+    },
+    "PolicyJson": {
+      "type": "String"
+    },
+    "AutomationAssumeRole": {
+      "type": "String"
+    }
   },
   "mainSteps": [
     {
       "action": "aws:executeAwsApi",
       "name": "AttachPolicy",
       "inputs": {
+        "RoleArn": "{{ AutomationAssumeRole }}",
         "Service": "StepFunctions",
         "Api": "PutResourcePolicy",
         "ResourceArn": "{{ ResourceArn }}",
@@ -31,15 +39,17 @@ resource "aws_ssm_document" "attach_sfn_policy" {
 DOC
 }
 
+
 resource "aws_ssm_association" "attach_policy_scheduled" {
-  name         = aws_ssm_document.attach_sfn_policy.name
-  schedule_expression = "rate(12 hours)"
-  association_name   = "AttachPolicyAutomation"
-  
+  name                       = aws_ssm_document.attach_sfn_policy.name
+  schedule_expression        = "rate(12 hours)"
+  association_name           = "AttachPolicyAutomation"
   automation_target_parameter_name = "ResourceArn"
 
   parameters = {
     "ResourceArn"          = aws_sfn_state_machine.manual_workflow.arn
     "PolicyJson"           = data.aws_iam_policy_document.cross_account_sfn_resource_policy.json
+    
+    "AutomationAssumeRole" = aws_iam_role.ssm_automation_role.arn
   }
 }
