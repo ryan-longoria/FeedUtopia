4b35ebad42bd1a4a0841f72cdf594e3e19e69f26
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-23 14:16:17 -0500

    Add requests dependency layer

--- accounts/wrestleutopia-prod/lambda.tf
+++ accounts/wrestleutopia-prod/lambda.tf
@@ -89,7 +89,8 @@ resource "aws_lambda_function" "process_content" {
   layers = [
     "arn:aws:lambda:us-east-2:825765422855:layer:imagick-layer:2",
     "arn:aws:lambda:us-east-2:580247275435:layer:LambdaInsightsExtension:14",
-    "arn:aws:lambda:us-east-2:825765422855:layer:Python_bs4:1"
+    "arn:aws:lambda:us-east-2:825765422855:layer:Python_bs4:1",
+    "arn:aws:lambda:us-east-2:825765422855:layer:Python_Requests:1"
   ]
 
   dead_letter_config {
