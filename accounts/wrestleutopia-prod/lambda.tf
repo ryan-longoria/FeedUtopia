3e329f08bf0f44c65cda97887a92b9ddbf830dc2
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-23 14:00:44 -0500

    Add feedparser dependency layer

--- accounts/wrestleutopia-prod/lambda.tf
+++ accounts/wrestleutopia-prod/lambda.tf
@@ -16,7 +16,8 @@ resource "aws_lambda_function" "fetch_data" {
   timeout          = 10
 
   layers = [
-    "arn:aws:lambda:us-east-2:580247275435:layer:LambdaInsightsExtension:14"
+    "arn:aws:lambda:us-east-2:580247275435:layer:LambdaInsightsExtension:14",
+    "arn:aws:lambda:us-east-2:825765422855:layer:Python_FeedParser:1"
   ]
 
   dead_letter_config {
