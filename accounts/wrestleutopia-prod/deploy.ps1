1ca6c893e3f937416aca6f27113528e94de5bd02
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-22 23:29:43 -0500

    Fill in placeholders

--- accounts/wrestleutopia-prod/deploy.ps1
+++ accounts/wrestleutopia-prod/deploy.ps1
@@ -1,11 +1,11 @@
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 825765422855.dkr.ecr.us-east-2.amazonaws.com
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 390402544450.dkr.ecr.us-east-2.amazonaws.com
 
 Set-Location -Path "artifacts/scripts/render_video"
 
 docker buildx build --platform=linux/amd64 --provenance=false --load -t render_video_image:latest .
 
-docker tag render_video_image:latest 825765422855.dkr.ecr.us-east-2.amazonaws.com/render_video_repository:latest
+docker tag render_video_image:latest 390402544450.dkr.ecr.us-east-2.amazonaws.com/render_video_repository:latest
 
-docker push 825765422855.dkr.ecr.us-east-2.amazonaws.com/render_video_repository:latest
+docker push 390402544450.dkr.ecr.us-east-2.amazonaws.com/render_video_repository:latest
 
 Set-Location -Path "../../.."
