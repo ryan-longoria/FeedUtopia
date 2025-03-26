6844de44f86095fd650c37757043b2fdfe4554c4
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-26 17:55:02 -0500

    Add Cloudwatch dashboard for my API Gateway

--- accounts/sharedservices-prod/dashboard.tf
+++ accounts/sharedservices-prod/dashboard.tf
@@ -84,3 +84,86 @@ resource "aws_cloudwatch_dashboard" "step_functions_dashboard" {
     ]
   })
 }
+
+#####################################################################
+# CloudWatch Dashboard Example (JSON Encoded)
+#####################################################################
+
+resource "aws_cloudwatch_dashboard" "api_monitoring_dashboard" {
+  dashboard_name = "CrossAccountStateMachineDashboard"
+
+  dashboard_body = jsonencode({
+    start   = "-3H"
+
+    widgets = [
+      {
+        "type" : "metric",
+        "x" : 0,
+        "y" : 0,
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "title" : "API Gateway 4XX/5XX Errors",
+          "view" : "timeSeries",
+          "region" : var.aws_region,
+          "stacked" : false,
+          "period" : 60,
+          "metrics" : [
+            [
+              "AWS/ApiGateway",
+              "4XXError",
+              "ApiName",
+              aws_api_gateway_rest_api.api.name,
+              "Stage",
+              aws_api_gateway_stage.api_stage.stage_name,
+              { "stat" : "Sum", "period" : 60 }
+            ],
+            [
+              "AWS/ApiGateway",
+              "5XXError",
+              "ApiName",
+              aws_api_gateway_rest_api.api.name,
+              "Stage",
+              aws_api_gateway_stage.api_stage.stage_name,
+              { "stat" : "Sum", "period" : 60 }
+            ]
+          ]
+        }
+      },
+
+      {
+        "type" : "metric",
+        "x" : 12,
+        "y" : 0,
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "title" : "API Gateway Latency",
+          "view" : "timeSeries",
+          "region" : var.aws_region,
+          "period" : 60,
+          "metrics" : [
+            [
+              "AWS/ApiGateway",
+              "Latency",
+              "ApiName",
+              aws_api_gateway_rest_api.api.name,
+              "Stage",
+              aws_api_gateway_stage.api_stage.stage_name,
+              { "stat" : "Average" }
+            ],
+            [
+              "AWS/ApiGateway",
+              "IntegrationLatency",
+              "ApiName",
+              aws_api_gateway_rest_api.api.name,
+              "Stage",
+              aws_api_gateway_stage.api_stage.stage_name,
+              { "stat" : "Average" }
+            ]
+          ]
+        }
+      }
+    ]
+  })
+}
