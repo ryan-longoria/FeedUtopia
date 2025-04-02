6a158a0cb9212c6672813a9841d26ae0286b0840
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-04-01 20:08:15 -0500

    Add new provider assuming a role in sharedservices

--- accounts/animeutopia-prod/route53.tf
+++ accounts/animeutopia-prod/route53.tf
@@ -3,11 +3,13 @@
 ################################################################################
 
 data "aws_route53_zone" "accountstore" {
-  name       = "animeutopiastore.com"
+  provider     = aws.dns
+  name         = "animeutopiastore.com"
   private_zone = false
 }
 
 resource "aws_route53_record" "account_domain_a" {
+  provider = aws.dns
   zone_id  = data.aws_route53_zone.accountstore.zone_id
   name     = "animeutopiastore.com"
   type     = "A"
