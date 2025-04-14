dbf7f3682e83c5214f9224fd916dcfa52cdd6380
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-04-14 15:52:50 -0500

    Add two lines next to logo

--- accounts/sharedservices-prod/artifacts/scripts/render_video/lambda_function.py
+++ accounts/sharedservices-prod/artifacts/scripts/render_video/lambda_function.py
@@ -9,7 +9,7 @@ from moviepy.video.VideoClip import ColorClip, ImageClip, TextClip
 from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
 from moviepy.video.io.VideoFileClip import VideoFileClip
 import moviepy.video.fx as vfx
-from PIL import ImageFont
+from PIL import ImageFont, ImageColor
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -318,21 +318,54 @@ def create_artifact_clip(spinning_artifact: str, bucket_name: str) -> Optional[V
     return None
 
 
-def create_logo_clip(bucket_name: str, duration_sec: float) -> Optional[ImageClip]:
+def create_logo_clip(bucket_name: str, duration_sec: float) -> Optional[CompositeVideoClip]:
     """
-    Download and resize a logo overlay.
+    Download and resize a logo overlay, then add thin horizontal lines on each side.
     """
     logo_key = "artifacts/Logo.png"
     downloaded_logo = download_s3_file(bucket_name, logo_key, LOCAL_LOGO)
-    if downloaded_logo and os.path.exists(LOCAL_LOGO):
-        raw_logo = ImageClip(LOCAL_LOGO)
-        scale_logo = 200 / raw_logo.w
-        logo_clip = raw_logo.with_effects([vfx.Resize(scale_logo)]).with_duration(duration_sec)
-        position_x = DEFAULT_VIDEO_WIDTH - logo_clip.w - 150
-        position_y = DEFAULT_VIDEO_HEIGHT - logo_clip.h - 100
-        logo_clip = logo_clip.with_position((position_x, position_y))
-        return logo_clip
-    return None
+    if not (downloaded_logo and os.path.exists(LOCAL_LOGO)):
+        return None
+
+    raw_logo = ImageClip(LOCAL_LOGO)
+    scale_logo = 200 / raw_logo.w
+    logo_clip = raw_logo.with_effects([vfx.Resize(scale_logo)]).with_duration(duration_sec)
+
+    line_width_left = 300
+    line_width_right = 300
+    line_height = 4
+    hex_color = "#ec008c"
+    line_color = ImageColor.getrgb(hex_color)
+
+    line_left = ColorClip(size=(line_width_left, line_height), color=line_color).with_duration(duration_sec)
+    line_right = ColorClip(size=(line_width_right, line_height), color=line_color).with_duration(duration_sec)
+
+    gap_between_line_and_logo = 20
+    total_width = line_width_left + gap_between_line_and_logo + logo_clip.w + gap_between_line_and_logo + line_width_right
+    total_height = max(line_height, logo_clip.h)
+
+    line_left_x = 0
+    line_left_y = (total_height - line_height) // 2
+
+    logo_x = line_width_left + gap_between_line_and_logo
+    logo_y = (total_height - logo_clip.h) // 2
+
+    line_right_x = logo_x + logo_clip.w + gap_between_line_and_logo
+    line_right_y = (total_height - line_height) // 2
+
+    composite_with_lines = CompositeVideoClip(
+        [
+            line_left.with_position((line_left_x, line_left_y)),
+            logo_clip.with_position((logo_x, logo_y)),
+            line_right.with_position((line_right_x, line_right_y)),
+        ],
+        size=(total_width, total_height),
+    ).with_duration(duration_sec)
+
+    final_x = (DEFAULT_VIDEO_WIDTH - total_width) / 2
+    final_y = DEFAULT_VIDEO_HEIGHT - composite_with_lines.h - 100
+
+    return composite_with_lines.with_position((final_x, final_y))
 
 
 def create_gradient_clip(bucket_name: str, duration_sec: float) -> Optional[ImageClip]:
