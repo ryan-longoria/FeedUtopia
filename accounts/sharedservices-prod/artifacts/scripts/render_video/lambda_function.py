f9bbe556d7ff7129abb0f845b0c6ed63e32b5284
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-04-14 20:32:11 -0500

    Lower text width

--- accounts/sharedservices-prod/artifacts/scripts/render_video/lambda_function.py
+++ accounts/sharedservices-prod/artifacts/scripts/render_video/lambda_function.py
@@ -401,12 +401,12 @@ def create_text_clips(
         if spinning_artifact == "TRAILER":
             top_font_size = dynamic_font_size(title_text, 125, 75, 25)
             subtitle_font_size = dynamic_font_size(description_text, 70, 30, 45)
-            title_max_width = 900
+            title_max_width = 800
             subtitle_max_width = 800
         elif spinning_artifact in ["NEWS", "FACT"]:
             top_font_size = dynamic_font_size(title_text, 100, 70, 30)
             subtitle_font_size = dynamic_font_size(description_text, 70, 25, 45)
-            title_max_width = 900
+            title_max_width = 800
             subtitle_max_width = 800
         else:
             top_font_size = dynamic_font_size(title_text, 100, 70, 30)
