28ff28029bc3e5c7975348979edf4d9f45a1a772
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-04-28 14:45:45 -0500

    Extend fetch_data function timeout to 30

--- accounts/animeutopia-prod/lambda.tf
+++ accounts/animeutopia-prod/lambda.tf
@@ -13,7 +13,7 @@ resource "aws_lambda_function" "fetch_data" {
   handler          = "lambda_function.lambda_handler"
   runtime          = "python3.9"
   role             = aws_iam_role.lambda_role.arn
-  timeout          = 5
+  timeout          = 30
 
   environment {
     variables = {
