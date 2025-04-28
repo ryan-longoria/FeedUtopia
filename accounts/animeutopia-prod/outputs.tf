6c7c1afaecfa4707a0941921f37f7b3e1acab2d2
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-04-28 18:38:31 -0500

    Output the gateway stage url

--- accounts/animeutopia-prod/outputs.tf
+++ accounts/animeutopia-prod/outputs.tf
@@ -1,3 +1,7 @@
 ################################################################################
 ## Outputs
 ################################################################################
+
+output "ann_proxy_invoke_url" {
+  value = "${aws_apigatewayv2_stage.ann_proxy_stage.invoke_url}/newsroom/rss.xml"
+}
