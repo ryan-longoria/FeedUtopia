5960515154ee85f5c0d88a5d2b0be1270d54be4e
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-04-28 18:53:53 -0500

    Add A record for rss.sheanime.com

--- accounts/animeutopia-prod/route53.tf
+++ accounts/animeutopia-prod/route53.tf
@@ -26,3 +26,27 @@ data "aws_route53_zone" "this" {
   name         = var.domain
   private_zone = false
 }
+
+resource "aws_route53_record" "ann_proxy_alias" {
+  zone_id = data.aws_route53_zone.this.id
+  name    = "rss.${var.domain}"
+  type    = "A"
+
+  alias {
+    name                   = aws_apigatewayv2_domain_name.ann_domain.domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.ann_domain.domain_name_configuration[0].hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "ann_proxy_alias_ipv6" {
+  zone_id = data.aws_route53_zone.this.id
+  name    = "rss.${var.domain}"
+  type    = "AAAA"
+
+  alias {
+    name                   = aws_apigatewayv2_domain_name.ann_domain.domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.ann_domain.domain_name_configuration[0].hosted_zone_id
+    evaluate_target_health = false
+  }
+}
