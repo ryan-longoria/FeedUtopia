57174c7d1a31739fd588184f6286855cb22960a7
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-04-29 07:41:51 -0500

    Add feedparser dependency to fetch_data

--- accounts/animeutopia-prod/lambda.tf
+++ accounts/animeutopia-prod/lambda.tf
@@ -25,8 +25,7 @@ resource "aws_lambda_function" "fetch_data" {
 
   layers = [
     "arn:aws:lambda:us-east-2:580247275435:layer:LambdaInsightsExtension:14",
-    "arn:aws:lambda:us-east-2:825765422855:layer:Python_bs4:1",
-    "arn:aws:lambda:us-east-2:825765422855:layer:Python_cloudscraper:1"
+    "arn:aws:lambda:us-east-2:825765422855:layer:Python_FeedParser:1"
   ]
 
   dead_letter_config {
