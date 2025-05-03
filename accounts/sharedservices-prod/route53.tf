c454d32af008310739cda76f85a997ea3bcd09c4
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-05-03 10:27:20 -0500

    Create A record for privacy policy site

--- accounts/sharedservices-prod/route53.tf
+++ accounts/sharedservices-prod/route53.tf
@@ -23,3 +23,15 @@ resource "aws_route53_record" "api_feedutopia_alias" {
     evaluate_target_health = false
   }
 }
+
+resource "aws_route53_record" "privacy_record" {
+  zone_id = data.aws_route53_zone.feedutopia_zone.zone_id
+  name    = "privacy.feedutopia.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.privacy_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.privacy_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
