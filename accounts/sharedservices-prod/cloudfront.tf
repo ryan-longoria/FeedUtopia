28439bca0fcc62004370096ba14a6119fed5d6ce
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-05-03 11:12:59 -0500

    Add s3 website endpoint

--- accounts/sharedservices-prod/cloudfront.tf
+++ accounts/sharedservices-prod/cloudfront.tf
@@ -3,17 +3,17 @@
 ################################################################################
 
 resource "aws_cloudfront_distribution" "privacy_distribution" {
-  origin {
-    domain_name = aws_s3_bucket_website_configuration.privacy_website.website_endpoint
-    origin_id   = "S3-privacy-feedutopia"
+    origin {
+        domain_name = aws_s3_bucket_website_configuration.privacy_website.website_endpoint
+        origin_id   = "S3-privacy-feedutopia"
 
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1.2"]
+        custom_origin_config {
+            http_port              = 80
+            https_port             = 443
+            origin_protocol_policy = "http-only"
+            origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+        }
     }
-  }
 
   enabled             = true
   is_ipv6_enabled     = true
