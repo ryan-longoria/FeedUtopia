ef33fa1f9e8c0b7691a5e672687b20f227efcd94
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-05-03 21:44:09 -0500

    Add Instagram Oauth handler

--- accounts/sharedservices-prod/lambda.tf
+++ accounts/sharedservices-prod/lambda.tf
@@ -236,8 +236,8 @@ resource "aws_lambda_function" "instagram_oauth" {
   runtime       = "python3.9"
   role          = aws_iam_role.lambda_role.arn
 
-  filename         = data.archive_file.oauth_zip.output_path
-  source_code_hash = data.archive_file.oauth_zip.output_base64sha256
+  filename         = "${path.module}/artifacts/scripts/instagram_oauth/instagram_oauth.zip"
+  source_code_hash = filebase64sha256("${path.module}/artifacts/scripts/instagram_oauth/instagram_oauth.zip")
 
   environment {
     variables = {
