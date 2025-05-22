e798368bf1f0d538a14e525a83f87637ef32ebc6
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-05-22 08:43:38 -0500

    Add Entra ID authentication Lambda@Edge module

---
+++ accounts/sharedservices-prod/module.tf
@@ -0,0 +1,16 @@
+################################################################################
+## Azure Entra ID module
+################################################################################
+
+module "edge_auth" {
+  source  = "nickshine/lambda-edge-azure-auth/aws"
+  version = "0.3.3"
+
+  client_id     = var.azuread_client_id
+  client_secret = var.azuread_client_secret
+  tenant        = var.azuread_tenant_id
+  redirect_uri  = "https://feedutopia.com/_auth/callback"
+
+  session_duration = 24
+  simple_urls_enabled = true
+}
