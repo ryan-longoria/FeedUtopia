c828c8b1751c5be78414a98db81a70c8c27e9fbb
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-05-23 09:57:30 -0500

    Add ssm parameters

--- accounts/animeutopia-prod/lambda.tf
+++ accounts/animeutopia-prod/lambda.tf
@@ -18,8 +18,8 @@ resource "aws_lambda_function" "fetch_data" {
   environment {
     variables = {
       TEAMS_WEBHOOK_URL = local.TEAMS_WEBHOOK_URL,
-      ANN_NEWS_USER = var.ann_news_user
-      ANN_NEWS_PASS = var.ann_news_pass
+      ANN_NEWS_USER = data.aws_ssm_parameter.ann_news_user.value
+      ANN_NEWS_PASS = data.aws_ssm_parameter.ann_news_pass.value
     }
   }
 
--- accounts/sharedservices-prod/ssm.tf
+++ accounts/sharedservices-prod/ssm.tf
@@ -6,3 +6,18 @@ data "aws_ssm_parameter" "openai" {
   name            = "/openai/api_key"
   with_decryption = true
 }
+
+data "aws_ssm_parameter" "instagram_verify_token" {
+  name            = "/instagram/verifytoken"
+  with_decryption = true
+}
+
+data "aws_ssm_parameter" "instagram_app_id" {
+  name            = "/instagram/client_id"
+  with_decryption = true
+}
+
+data "aws_ssm_parameter" "instagram_app_secret" {
+  name            = "/instagram/client_secret"
+  with_decryption = true
+}
