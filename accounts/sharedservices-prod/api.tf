1927782abb425dfd043b315acee4121da465aeeb
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-05-23 06:48:00 -0500

    Add CORS helper for route /gpt/ig-caption

--- accounts/sharedservices-prod/api.tf
+++ accounts/sharedservices-prod/api.tf
@@ -695,6 +695,50 @@ resource "aws_api_gateway_integration" "gpt_ig_caption_int" {
   uri                     = aws_lambda_function.gpt_ig_caption.invoke_arn
 }
 
+resource "aws_api_gateway_method" "gpt_ig_caption_options" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.gpt_ig_caption.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "gpt_ig_caption_options_mock" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.gpt_ig_caption.id
+  http_method = aws_api_gateway_method.gpt_ig_caption_options.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_method_response" "gpt_ig_caption_options_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.gpt_ig_caption.id
+  http_method = aws_api_gateway_method.gpt_ig_caption_options.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "gpt_ig_caption_options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.gpt_ig_caption.id
+  http_method = aws_api_gateway_method.gpt_ig_caption_options.http_method
+  status_code = aws_api_gateway_method_response.gpt_ig_caption_options_response.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key'"
+    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
+
 #############################
 # Instagram API Callback/Oauth
 #############################
