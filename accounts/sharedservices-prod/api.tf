8accabea6ed5f33a3c9e134c77b0517a8bef3eb1
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-05-23 19:20:27 -0500

    Add a gateway response for 5XX and 4XXs to inject CORS

--- accounts/sharedservices-prod/api.tf
+++ accounts/sharedservices-prod/api.tf
@@ -805,6 +805,29 @@ resource "aws_api_gateway_integration_response" "gpt_image_gen_options_integrati
   }
 }
 
+resource "aws_api_gateway_gateway_response" "default_5xx_cors" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  response_type = "DEFAULT_5XX"
+
+  response_parameters = {
+    # note the single quotes inside the string
+    "gatewayresponse.header.Access-Control-Allow-Origin"  = "'*'"
+    "gatewayresponse.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key'"
+    "gatewayresponse.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
+  }
+}
+
+resource "aws_api_gateway_gateway_response" "default_4xx_cors" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  response_type = "DEFAULT_4XX"
+
+  response_parameters = {
+    "gatewayresponse.header.Access-Control-Allow-Origin"  = "'*'"
+    "gatewayresponse.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key'"
+    "gatewayresponse.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
+  }
+}
+
 #############################
 # Instagram API Callback/Oauth
 #############################
