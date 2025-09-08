3b5d2b402dc23666cdc0a246430ce68aded744f8
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-09-08 17:22:42 -0500

    Remove amplify.yml

--- accounts/wrestleutopia-prod/artifacts/website/vite.config.js
+++ accounts/wrestleutopia-prod/artifacts/website/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: '.',                 // project root
   build: {
-    outDir: 'public/assets', // emit built files where your site can serve them
+    outDir: 'dist', // emit built files where your site can serve them
     emptyOutDir: false,      // don't wipe the rest of public/
     rollupOptions: {
       input: {
