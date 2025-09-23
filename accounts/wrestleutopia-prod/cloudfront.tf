3e62586e975b946975ddad5a7916db31a2c8b002
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-09-22 21:25:51 -0500

    Make S3 objects public

---
+++ accounts/wrestleutopia-prod/cloudfront.tf
@@ -0,0 +1,49 @@
+resource "aws_cloudfront_origin_access_control" "media" {
+  name                              = "media-oac"
+  description                       = "OAC for ${aws_s3_bucket.media_bucket.bucket}"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "media" {
+  enabled = true
+  comment = "WrestleUtopia Media CDN"
+
+  origin {
+    domain_name              = local.s3_origin_domain
+    origin_id                = "media-s3-origin"
+
+    s3_origin_config {
+      origin_access_identity = ""
+    }
+
+    origin_access_control_id = aws_cloudfront_origin_access_control.media.id
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "media-s3-origin"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+
+    forwarded_values {
+      query_string = false
+      cookies { forward = "none" }
+    }
+
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction { restriction_type = "none" }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  depends_on = [aws_cloudfront_origin_access_control.media]
+}
--- accounts/wrestleutopia-prod/locals.tf
+++ accounts/wrestleutopia-prod/locals.tf
@@ -6,4 +6,5 @@ locals {
   project           = "${var.project_name}-prod"
   TEAMS_WEBHOOK_URL = var.teams_webhooks[var.project_name].auto
   user_pool_name = "${var.project_name}-user-pool"
+  s3_origin_domain = "${aws_s3_bucket.media_bucket.bucket}.s3.${var.aws_region}.amazonaws.com"
 }
