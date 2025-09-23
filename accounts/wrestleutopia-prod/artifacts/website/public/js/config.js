77f25536e5c3edfc49cb59a2d8977f7a8283f102
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-09-22 22:15:22 -0500

    Make S3 objects public

--- accounts/wrestleutopia-prod/artifacts/website/public/js/config.js
+++ accounts/wrestleutopia-prod/artifacts/website/public/js/config.js
@@ -1,2 +1,2 @@
 window.WU_API = "https://go2gft4394.execute-api.us-east-2.amazonaws.com";
-window.WU_MEDIA_BASE = "d178p8k1vmj1zs.cloudfront.net";
+window.WU_MEDIA_BASE = "https://d178p8k1vmj1zs.cloudfront.net";
